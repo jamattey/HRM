9a6958b5ade572c2c88e6f9c8b4d30e1f0082fa7
James Amattey <me@jamattey.com>
2022-04-11 04:02:47 +0000

    Create ILeaveTypeRepository.cs

---
+++ src/Core/HRM.Application/Persistence/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HRM.Domain;
+
+
+namespace HRM.Application.Persistence.Contracts
+{
+    public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+    {
+        
+    }
+}
