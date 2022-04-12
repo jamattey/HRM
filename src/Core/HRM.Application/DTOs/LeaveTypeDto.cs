88bf03e2b8c5f1de7fcb702cc96e8f6c13456a52
James Amattey <me@jamattey.com>
2022-04-12 01:18:41 +0000

    Create LeaveTypeDto.cs
    A data transfer object (DTO) is an object that carries data between processes.

---
+++ src/Core/HRM.Application/DTOs/LeaveTypeDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HR.Application.DTOs.Common;
+
+namespace HRM.Application.DTOs
+{
+    public class LeaveTypeDto : BaseDto
+    {
+
+    }
+}
