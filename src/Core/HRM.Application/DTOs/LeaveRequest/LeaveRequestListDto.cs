b7c5d8255982f790a9323709053859e7f850b7bc
James Amattey <me@jamattey.com>
2022-04-12 02:47:42 +0000

    Create LeaveRequestListDto.cs

---
+++ src/Core/HRM.Application/DTOs/LeaveRequest/LeaveRequestListDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRM.Application.DTOs.LeaveRequest
+{
+    public class LeaveRequestListDto : BaseDto
+    {
+        public class LeaveRequestListDto
+        {
+            
+        }
+    }
+}
