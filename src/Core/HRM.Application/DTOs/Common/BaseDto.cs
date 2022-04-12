6fa5200f5bac320c2169b8bac86b0f2d3a1a8d91
James Amattey <me@jamattey.com>
2022-04-12 01:09:11 +0000

    Create BaseDto.cs
    BaseDTO contains common fields shared by DTO classes.

---
+++ src/Core/HRM.Application/DTOs/Common/BaseDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace HRM.Application.DTOs.Common
+{
+    public abstract class BaseDto
+    {
+        public int ID { get; set; }
+    }
+}
