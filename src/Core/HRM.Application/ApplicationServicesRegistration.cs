3f05ec58325365d6f34bce99ad677aa8ecdd67a2
James Amattey <me@jamattey.com>
2022-04-12 03:22:07 +0000

    Update ApplicationServicesRegistration.cs

--- src/Core/HRM.Application/ApplicationServicesRegistration.cs
+++ src/Core/HRM.Application/ApplicationServicesRegistration.cs
@@ -1,11 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using HRM.Application.Profiles;
+using MediatR;
 
 namespace HRM.Application
 {
     public class ApplicationServicesRegistration
     {
-        
+        public static IServiceCollection ConfigurationApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutomapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
     }
 }
